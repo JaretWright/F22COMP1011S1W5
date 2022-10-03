d099a07ba8cf4f419183f3b938e34e199ff4ba64
JaretWright <jaret.wright@georgiancollege.ca>
2022-10-03 11:39:05 -0400

    added tableview information

--- src/main/java/com/example/f22comp1011s1w1/SongTableViewController.java
+++ src/main/java/com/example/f22comp1011s1w1/SongTableViewController.java
@@ -2,10 +2,29 @@ package com.example.f22comp1011s1w1;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import java.io.IOException;
 
 public class SongTableViewController {
+
+    @FXML
+    private TableColumn<Song, String> artistColumn;
+
+    @FXML
+    private TableColumn<Song, String> genreColumn;
+
+    @FXML
+    private TableColumn<Song, Integer> lengthColumn;
+
+    @FXML
+    private TableColumn<Song, Integer> songIDColumn;
+
+    @FXML
+    private TableColumn<Song, String> songNameColumn;
+
+    @FXML
+    private TableView<Song> tableview;
 
     @FXML
     void createSongScene(ActionEvent event) throws IOException {
